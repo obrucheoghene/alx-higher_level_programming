bdeea507b43255fe43d287577bcde20bfd50be97
Wilfred <wilfredcloudspace@gmail.com>
2023-01-06 17:49:37 +0100

    create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str)
+print(str[39:66] + " with python")
