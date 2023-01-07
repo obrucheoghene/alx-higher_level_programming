e8845a21dfb6829ca7579fb19008c09c9df8bb0b
Wilfred <wilfredcloudspace@gmail.com>
2023-01-07 10:35:55 +0100

    script to check if linked list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check is a singly linked list has a cycle in it
+ * @list: pointer to the head
+ *
+ * Return: 0 if there is no cylce, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current;
+
+	current = list->next;
+
+	while (current)
+	{
+		if (current == list)
+		{
+			return (1);
+		}
+		current = current->next;
+	}
+
+	return (0);
+}
