0caa27d2750d1a2089e291117278ece549debb9c
Wilfred <wilfredcloudspace@gmail.com>
2023-01-09 17:58:57 +0100

    Adds script that replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replaces an element in a list at a specific position without modifying the original list"""
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+ 
+    copy = [x for x in my_list]
+    copy[idx] = element 
+
+    return copy
