5b227810215396b49d0c08bf7bf8e3ee83e04e10
Wilfred <wilfredcloudspace@gmail.com>
2023-01-09 02:53:30 +0100

    a scripts that prints all possible different combination of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digit in range(10):
+    for second_digit in range(digit + 1, 10):
+        if digit == 8 and second_digit == 9:
+            print("{}{}".format(digit, second_digit))
+        else:
+            print ("{}{}".format(digit, second_digit), end=", ")
