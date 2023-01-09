d769ef357776361c42a9ac0c782bbdd8cc21fbd9
Wilfred <wilfredcloudspace@gmail.com>
2023-01-09 20:11:35 +0100

    Adds scripts that prints a matrix integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers"""
+
+    for row in matrix:
+        for i in range(len(row)):
+            if i == (len(row) - 1):
+                print("{:d}".format(row[i]))
+            else:
+                 print("{:d}".format(row[i]), end=", ")
