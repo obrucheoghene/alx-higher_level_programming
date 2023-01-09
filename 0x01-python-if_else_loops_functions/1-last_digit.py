b995282b60fc361d464cb647e0c8186330bd4763
Wilfred <wilfredcloudspace@gmail.com>
2023-01-09 01:15:07 +0100

    add - to last digit of negative number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,10 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 string = "Last digit of"
 
-if last_digit == 0:
+if number < 0:
+    last_digit = -last_digit
+    print(f"{string} {number} is {last_digit} and is less than 6 and not 0")
+elif last_digit == 0:
     print(f"{string} {number} is {last_digit} and is 0")
 elif last_digit > 5:
     print(f"{string} {number} is {last_digit} and is greater than 5")
