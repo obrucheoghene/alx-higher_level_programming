54818583c1c468a17a6678b92eacace1d4ae03c4
Wilfred <wilfredcloudspace@gmail.com>
2023-01-10 01:39:27 +0100

    Adds not list condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Prints all integer of a list in reverse order"""
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+    if not my_list:
+        pass
+    else:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
