558daf947554c6a7803b2b9839686608434c6031
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 05:12:24 +0100

    Move 6-print_sorted_dictionary

--- 0x02-python-import_modules/6-print_sorted_dictionary.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    keys_list = sorted(list(a_dictionary.keys()))
-    print(keys_list);
-
-    for x in keys_list:
-        print("{}: {}".format(x, a_dictionary[x]))
