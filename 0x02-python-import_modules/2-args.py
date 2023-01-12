2e39ecab806875869c35402102f77d173dd13c46
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 03:56:54 +0100

    Fix indentation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ != "__main__":
 if len(sys.argv) == 1:
     print("{:d} arguments.".format(0))
 elif len(sys.argv) == 2:
-   print("{:d} argument:".format(1))
-   print("{:d}: {}".format(1, sys.argv[1]))
+    print("{:d} argument:".format(1))
+    print("{:d}: {}".format(1, sys.argv[1]))
 else:
     print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
