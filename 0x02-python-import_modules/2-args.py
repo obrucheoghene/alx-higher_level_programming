4817db4db0d252392313663baf646b8f12a0635b
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 03:43:31 +0100

    Add scripts that prints the number of and the list of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) == 1:
+    print("{:d} arguments".format(0))
+else:
+    print("{:d} arguments".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {}".format(i, sys.argv[i]))
+
