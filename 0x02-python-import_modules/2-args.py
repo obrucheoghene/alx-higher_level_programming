30facfcabda8abdb5cddb6397e692b9e7675e382
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 03:55:48 +0100

    Meet requirement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,13 @@
 import sys
 
 if __name__ != "__main__":
-    sys.exit();
+    sys.exit()
 
 if len(sys.argv) == 1:
     print("{:d} arguments.".format(0))
+elif len(sys.argv) == 2:
+   print("{:d} argument:".format(1))
+   print("{:d}: {}".format(1, sys.argv[1]))
 else:
     print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
