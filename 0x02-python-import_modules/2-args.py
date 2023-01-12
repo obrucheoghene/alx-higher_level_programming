6d56f440f2bc73032edc551e90646c8da6d2175c
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 03:51:12 +0100

    Fix requirement output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ != "__main__":
     sys.exit();
 
 if len(sys.argv) == 1:
-    print("{:d} arguments".format(0))
+    print("{:d} arguments.".format(0))
 else:
-    print("{:d} arguments".format(len(sys.argv) - 1))
+    print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{:d}: {}".format(i, sys.argv[i]))
