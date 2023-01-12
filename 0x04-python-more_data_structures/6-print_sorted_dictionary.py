8cc8f248edc808f6e768c69826e355e52e25d0e1
Wilfred <wilfredcloudspace@gmail.com>
2023-01-12 05:18:22 +0100

    Fix typeError

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     keys_list = sorted(list(a_dictionary.keys()))
-    print(keys_list)
 
     for x in keys_list:
-        print("{}: {}".format(x, a_dictionary[x]))
+        print("{}: {}".format(x, a_dictionary.get(x)))
