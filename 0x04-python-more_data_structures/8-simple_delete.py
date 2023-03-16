b369515174587fbb3b1bcbaa16f3aa593497dd74
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-16 03:07:49 +0100

    Fix keyError

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,5 @@
 def simple_delete(a_dictionary, key=""):
     """a function that deletes a key in a dictionary."""
-    del a_dictionary[key]
+    if key in a_dictionary:
+       del a_dictionary[key]
     return a_dictionary
