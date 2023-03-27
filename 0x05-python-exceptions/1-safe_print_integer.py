33e970ac631abd62de4002d4c16760f47d4421af
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-27 21:34:19 +0100

    Add python hashbang to first line

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 def safe_print_integer(value):
     """
-     function that prints an integer with "{:d}".format()
+     function that prints an integer with
+     "{:d}".format()
     """
     try:
         print("{:d}".format(value))
