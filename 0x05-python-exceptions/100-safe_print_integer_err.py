70a6982bd8ae3900e2df97d745c2c9cfc7f0faac
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-28 01:18:07 +0100

    Add  function that executes a function safely.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (TypeError, ValueError) as err:
-        print("Exception: {}".format(err))
+        print("Exception: {}".format(err), file=stderr)
         return False
