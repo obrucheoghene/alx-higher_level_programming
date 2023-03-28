5055f445b3870fe2a35ca7f88d5efea6be411431
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-28 23:14:12 +0100

    Add 0 to size as default value

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """Square class task 1"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize the square class"""
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
