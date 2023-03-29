2ce18f5fdf99f46064505a861e323667f6f73bd2
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-30 00:20:01 +0100

    Add a node and singlist class

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""A mode that defines a node and singly linked list classes"""
+
+
+class Node:
+    """A that defines the node of a singly linked list"""
+
+    def __init__(self, data, next_node=None):
+        """Node construtor"""
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """Get node data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Set node data"""
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """Get next node"""
+        self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Set next node"""
+        if type(value) is not type(self) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """A class that defines a singly linked list"""
+
+    def __init__(self):
+        """Contruct initialized object"""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """A method that inserts a new Node into
+        the correct sorted position in the
+        list (increasing order)
+        """
+        if self.__head is None:
+            self.__head = Node(value)
+        elif self.__head.data > value:
+            self.__head = Node(value, self.__head)
+        else:
+            temp = self.__head
+            while temp.next_node is not None and temp.next_node.data < value:
+                temp = temp.next_node
+
+            temp.next_node = Node(value, temp.next_node)
+
+    def __str__(self):
+        """Class string methon"""
+        temp = self.__head
+        values = []
+        while temp is not None:
+            values.append(str(temp.data))
+            temp = temp.next_node
+        return ('\n'.join(values))
