1f13e57045e022e4a4d0a4d6465290caef2f39a6
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-29 01:20:05 +0100

    Add a class to access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,8 +5,13 @@
 class Square:
     """Square class task 1"""
     def __init__(self, size=0):
-        """Initialize the square class"""
-        self.__size = size
+        """Initialize the square class""" 
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """Calculates the area of the square"""
