c16866bcdde14d0aa78c81b0dcafe5dbcbb88fd8
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-29 01:16:21 +0100

    Add a class to access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A module that defines a square class"""
+
+
+class Square:
+    """Square class task 1"""
+    def __init__(self, size=0):
+        """Initialize the square class"""
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Size getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Size setter method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
