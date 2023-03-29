7a27e5691b2fb99a5538be901d3b2aeb4c692fe0
Wilfred <wilfredcloudspace@gmail.com>
2023-03-29 22:24:04 +0100

    Add header

---
+++ 0x05-python-exceptions/101-python.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <object.h>
+#include <floatobject.h>
+#include <bytesobject.h>
+#include <listobject.h>
+#include <Python.h>
+
+/**
+ * print_python_list - print info on python list
+ */
+void print_python_list(PyObject *p)
+{
+}
