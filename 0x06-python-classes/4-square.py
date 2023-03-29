e3370db42342072b8720153186cfde0dad189c91
wilfredcloud <wilfredcloudspace@gmail.com>
2023-03-29 01:22:40 +0100

    Remove condition from __init__ method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,12 +6,7 @@ class Square:
     """Square class task 1"""
     def __init__(self, size=0):
         """Initialize the square class"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     def area(self):
         """Calculates the area of the square"""
