0d97a1d674d8ea550bc4a82ab0b220f60d7d69ff
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-03 15:03:15 +0100

    feat: Add area and perimeter to Rectangle class

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+"""
+A module that defines a Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    A Rectangle class.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Constructor for Recatangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        Get the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        Args:
+            value (int): The new width of the rectangle.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Get the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        Args:
+            value (int): The new height of the rectangle.
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Calcuate the area of the retangle.
+
+        Returns:
+            int: The area of the reactangle.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Calcuate the perimeter of the retangle.
+
+        Returns:
+            int: The perimeter of the reactangle.
+        """
+        if width == 0 or height == 0:
+            return 0
+        else:
+            return 2 * self.__width * self.__height
