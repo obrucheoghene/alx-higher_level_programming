85099beee835cd3f97136272696ba12fed358661
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-04 05:28:22 +0100

    Fix pyccodestyle error

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,7 +8,7 @@ placing N non-attacking queens on an N×N chessboard.
 
 
 import sys
-
+"""
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
@@ -34,11 +34,11 @@ def backtrack(r):
         copy = ["".join(row) for row in board]
         res.append(copy)
         return
-    
+
     for c in range(N):
         if c in col or (r + c) in pDiag or (r - c) in nDiag:
             continue
-        
+
         col.add(c)
         pDiag.add(r + c)
         nDiag.add(r - c)
@@ -49,6 +49,16 @@ def backtrack(r):
         pDiag.remove(r + c)
         nDial.remove(r - c)
         board[r][c] = "."
+"""
 
-backtrack(0)
-print(res)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
