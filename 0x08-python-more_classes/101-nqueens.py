dc99db4aa09a3ea0f0a683fca9ca96b5197aac6a
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-04 03:41:36 +0100

    Add check

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+"""
+This module solves the N queens problem.
+The N queens puzzle is the challenge of
+placing N non-attacking queens on an N×N chessboard.
+"""
+
+
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+N = sys.argv[1]
+
+if type(N) is not int:
+    print("N must be a number")
+    exit(1)
+if N < 4:
+    print("N must be at least 4")
+    exit(1)
