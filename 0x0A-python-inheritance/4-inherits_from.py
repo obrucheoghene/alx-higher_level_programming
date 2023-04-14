9730c417abd82f294ebf7f290e5722c3d6e46203
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-14 03:17:15 +0100

    Add a function that checks an object is subclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a function that returns true True if the object inherits
+from a specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if object inherits from a specified class
+
+    Args:
+        obj (object): The object to be checked
+        a_class (a_class): The super class
+
+    Return:
+        (bool): True if inherited, False otherwise
+    """
+    return issubclass(obj, a_class)
