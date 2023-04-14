c096963cefb0b0d89cd9fbfcaea8de4eb09113f0
wilfredcloud <wilfredcloudspace@gmail.com>
2023-04-14 03:40:56 +0100

    Add instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """This module defines a BaseGeometry"""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
