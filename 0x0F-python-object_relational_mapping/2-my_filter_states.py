ea000fa5755a8b3e11cba64bd782a3eaa024262f
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-18 18:07:47 +0100

    feat: Add script to select all by value

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
-#!usr/bin/python3
+#!/usr/bin/python3
+
 """
-A script that takes in an argument and displays
-all values in the states table of
-hbtn_0e_0_usa where name matches the argument.
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
 """
 
 
@@ -18,9 +18,12 @@ if __name__ == '__main__':
                          port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states\
-                    WHERE name LIKE BINARY 'N%'\
-                    ORDER BY id ASC")
+
+    sql = """ SELECT * FROM states
+          WHERE name LIKE BINARY '{}'
+          ORDER BY id ASC """.format(sys.argv[4])
+
+    cursor.execute(sql)
 
     data = cursor.fetchall()
 
