43119218dadd27c271f0b552d10c4254797d4239
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-19 18:12:07 +0100

    fix: Make func. and executable

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,22 +5,25 @@ all values in the states table of hbtn_0e_0_usa
 where name matches the argument. But this time,
 write one that is safe from MySQL injections!
 """
-
-
 import MySQLdb
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
+def filter_states_by_name():
+    """Filter states by name and prevent sql injection"""
     db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3], port=3306)
     cur = db.cursor()
-    match = sys.argv[4]
+    match = argv[4]
     cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    filter_states_by_name()
