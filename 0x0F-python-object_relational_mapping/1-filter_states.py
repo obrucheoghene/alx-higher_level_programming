07cba5cf53a7fb29b14d693afb916b7f2fa0ec34
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-19 17:52:57 +0100

    fix: Make executable

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,15 +3,30 @@
 import MySQLdb
 from sys import argv
 
+def list_all_states_N():
+    """List all states that starts with Uppercase N"""
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host='localhost',
+        port=3306,
+        charset="utf8"
+        )
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
+    cursor = db.cursor()
+
+    cursor.execute("""SELECT * FROM states WHERE name
                 LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
+
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
+
+
+if __name__ == '__main__':
+    list_all_states_N()
