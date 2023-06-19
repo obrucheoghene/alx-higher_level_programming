729dad8c9fb9b14cc803daea9a7f49863cef8df5
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-19 18:14:59 +0100

    fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -2,13 +2,12 @@
 """
 A script that lists all cities from the database hbtn_0e_4_usa
 """
-
-
 import sys
 import MySQLdb
 
 
-if __name__ == '__main__':
+def list_all_cities():
+    """List all cities"""
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
@@ -26,3 +25,7 @@ if __name__ == '__main__':
 
     cursor.close()
     db.close()
+
+
+if __name__ == '__main__':
+    list_all_cities()
