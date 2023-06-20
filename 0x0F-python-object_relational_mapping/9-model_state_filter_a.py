b91c8041d358230361b2e27724602804cba967ed
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-20 03:38:05 +0100

    fix: task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""Filter states by letter a"""
-from sys import argv
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-def filter_states_by_letter_a():
-    """Filter states by letter a"""
-
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1], argv[2], argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    states = session.query(State).filter(State.name.like('%a'))
-    for state in states:
-        print(state.id, state.name, sep=": ")
-
-
-if __name__ == "__main__":
-    filter_states_by_letter_a()
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
