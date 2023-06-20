7497976180d00c17fdbc469f4366f0faaac69a81
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-20 01:10:58 +0100

    feat: Add list first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects from the database
+Lists first State objects from the database
 """
 from sys import argv
 from model_state import Base, State
@@ -8,16 +8,19 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-def list_all_states():
-    """Lists all State objects from the database"""
+def list_first_state():
+    """Lists first State objects from the database"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id):
+    state = session.query(State).first()
+    if state is None:
+        print("Nothing")
+    else:
         print(state.id, state.name, sep=": ")
 
 
 if __name__ == "__main__":
-    list_all_states()
+    list_first_state()
