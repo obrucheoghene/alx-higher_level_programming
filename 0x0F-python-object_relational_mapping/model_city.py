dd7d1e2c3b78c29f88f170d31e78e0970ec53e3d
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-20 03:10:27 +0100

    fix: Remove error

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,9 +6,6 @@ from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
-
 
 class City(Base):
     """
