eb6cd11c5cd543d58002ff35a95a98ac436abe03
wilfredcloud <wilfredcloudspace@gmail.com>
2023-06-30 18:47:04 +0100

    feat: Use request module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+requests model
+"""
+import requests
+
+
+if __name__ == '__main__':
+    
+    html = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(html.text.__class__))
+    print("\t- content: {}".format(html.text))
