9fbc295e0e4afdae25a17d97dfbc430427793f73
wilfredcloud <wilfredcloudspace@gmail.com>
2023-07-01 19:24:16 +0100

    fix: Meet requirement

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,10 +5,10 @@ if (isNaN(myArgs[0])) {
   console.log('Missing size');
 } else {
   for (i = 0; i < parseInt(myArgs[0]); i++) {
-	let row = '';
-	for (j = 0; j < parseInt(myArgs[0]); j++) {
-	  row += 'X';
-	}
-	console.log(row);
+    let row = '';
+    for (j = 0; j < parseInt(myArgs[0]); j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 }
