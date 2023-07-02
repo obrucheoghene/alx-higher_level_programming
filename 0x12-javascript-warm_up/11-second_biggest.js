22f5e8560d54fee9b393c200e3c9a987a7c5f3e9
wilfredcloud <wilfredcloudspace@gmail.com>
2023-07-02 16:17:07 +0100

    feat: Update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2);
-if (!myArgs[0] || !myArgs[2]) {
+const process = require('process');
+const argv = process.argv;
+const argc = process.argv.length;
+if (argc < 4) {
   console.log(0);
 } else {
-  const args = myArgs.map(Number);
-  const firstMax = Math.max.apply(null, args);
-  args.splice(args.indexOf(firstMax), 1);
-  console.log(Math.max.apply(null, args));
+  const array = argv.slice(2).sort((a, b) => a - b).reverse();
+  console.log(array[1]);
 }
