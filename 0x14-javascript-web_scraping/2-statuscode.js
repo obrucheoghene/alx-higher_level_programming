91cf1bd47d73e5cfc45b91e0f2067d2483451647
wilfredcloud <wilfredcloudspace@gmail.com>
2023-07-26 04:09:23 +0100

    fix: update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,14 +2,9 @@
 const process = require('process');
 const request = require('request');
 
-if (process.argv.length < 3) {
-  process.stderr.write('Usage: ./2-statuscode.js <url>');
-  process.exit();
-}
-
 const url = process.argv[2];
 request(url, function (error, response) {
   if (!error) {
-    console.log('code: ', response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });
