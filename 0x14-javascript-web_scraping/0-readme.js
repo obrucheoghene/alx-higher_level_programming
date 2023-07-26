84282576632fd636173495fa515f2a60f8692556
wilfredcloud <wilfredcloudspace@gmail.com>
2023-07-26 03:35:05 +0100

    feat: Add readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const process = require('process');
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+  process.stderr.write('Usage: ./O-readme.js <filename>');
+  process.exit();
+}
+
+const file = process.argv[2];
+try {
+  const data = fs.readFileSync(file, 'utf8');
+  process.stdout.write(data);
+} catch (error) {
+  process.stderr.write(error);
+}
